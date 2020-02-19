ae9d6f24c534a30d38933b0fd8a4cfcbab05028c
jcope <jcope@redhat.com>
2020-02-19 17:18:52 -0600

    enable cert from files

--- main.go
+++ main.go
@@ -5,23 +5,50 @@ import (
 	"github.com/golang/glog"
 	"github.com/yard-turkey/cosi-prototype-interface/cosi"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"os"
 )
 
-func init(){
+const (
+	ENV_CERT_PATH = "COSI_CERT_PATH"
+	ENV_KEY_PATH  = "COSI_KEY_PATH"
+)
+
+var (
+	certPath string
+	keyPath  string
+)
+
+func init() {
 	flag.Parse()
+	var ok bool
+	certPath, ok = os.LookupEnv(ENV_CERT_PATH)
+	if ! ok {
+		glog.Fatalf("Env var %s not defined, required", ENV_CERT_PATH)
+	}
+	keyPath, ok = os.LookupEnv(ENV_KEY_PATH)
+	if ! ok {
+		glog.Fatal("env var %s not defined, required", ENV_KEY_PATH)
+	}
 }
 
 func main() {
 	glog.Info("starting s3 plugin services")
-	rpcServer := grpc.NewServer()
-	glog.Info("grpc server initialized")
+
+	glog.Infof("getting credentials from paths\n\tcert: %v\n\tkey: %v", certPath, keyPath)
+	creds, err := credentials.NewServerTLSFromFile(certPath, keyPath)
+	if err != nil {
+		glog.Fatalf("couldn't get credentials: %v", err)
+	}
+	rpcServer := grpc.NewServer(grpc.Creds(creds))
+
 	glog.Info("configuring provisioner server")
 	cosi.RegisterProvisionerServer(rpcServer, newHandler())
 	listener, err := configureHTTPListener()
 	if err != nil {
 		panic(err)
 	}
-	glog.Info("starting grpc server, waiting for requests")
+	glog.Info("starting provisioner server, waiting for requests")
 	if err = rpcServer.Serve(listener); err != nil {
 		panic(err)
 	}
